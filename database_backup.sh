6a600dc14d087eb8bd753ae5b64e5ac3f53c9216
Americo Chaquisse <achaquisse1@gmail.com>
2020-06-04 06:12:21 +0200

    uppdated bash script

--- database_backup.sh
+++ database_backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
 DATE=`date +%d%b%y`
-LOCAL_BACKUP_DIR="backup/"
+LOCAL_BACKUP_DIR="/opt/backup/"
 DB_HOST="127.0.0.1"
 DB_NAME="dbname"
 DB_USER="dbuser"
 DB_PASSWORD="password"
-LOG_FILE=backup/backup-$DATE.log
+LOG_FILE=$LOCAL_BACKUP_DIR/backup-$DATE.log
 BACKUP_SERVER_ENDPOINT="http://host/api/receiver/folder"
 
 ############### Local Backup  ########################
